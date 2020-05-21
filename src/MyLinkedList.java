e669f976e3a8b7e92b51681c30ad041e23f85556
Julius <Julius>
2020-05-21 22:51:05 +0300

    new commit

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,17 +1,18 @@
+
 /**
  *
  * @author Julius
  */
 public class MyLinkedList implements MyList_Interface {
-    
-        private Item head;
+
+    private Item head;
 
     public MyLinkedList() {
         head = null;
     }
 
     public void add(Object o) {
-        if(head == null) {
+        if (head == null) {
             this.head = new Item();
             this.head.value = o;
         } else {
@@ -23,36 +24,50 @@ public class MyLinkedList implements MyList_Interface {
             last.next.value = o;
         }
     }
+            
+    public Object get(int index) {
+        
+        return null;
+    }
 
     public void remove(int index) {
-        //
+        if (head == null) {
+            System.out.println("This array is empty..");
+        } else {
+
+        }
     }
 
     public void insert(int index, Object o) {
         //
     }
 
-    public String error() {
-      return null;  
+    public void set(int index, Object o) {
+        //
     }
+   
 
+    public String error() {
+        return null;
+    }
+    
     public int size() {
-        if(head == null) {
+        if (head == null) {
             return 0;
         } else {
             Item last = head;
             int counter = 1;
-            while(last.next != null) {
+            while (last.next != null) {
                 last = last.next;
                 counter++;
             }
-            
+
             return counter;
         }
     }
 
     public String toString() {
-         String s = "MyLinkedList{ ";
+        String s = "MyLinkedList{ ";
         Item last = head;
         while (last != null) {
             s += (last.value == null) ? "null" : last.value.toString() + " ";
@@ -61,11 +76,11 @@ public class MyLinkedList implements MyList_Interface {
         s += "}";
         return s;
     }
-    
-    
+
     private class Item {
+
         Item next;
         Object value;
     }
-    
+
 }
--- src/MyList_Interface.java
+++ src/MyList_Interface.java
@@ -6,6 +6,7 @@
 public interface MyList_Interface {
 
     public void add(Object o);
+    public Object get(int index);
     public void remove(int index);
     public void insert(int index, Object o);
     public String error();
